bd342d7cee8708e09f286fd0cc091aa6b928b42d
guxinghao <846061943@qq.com>
2019-12-13 14:29:59 +0800

    修改联系我们

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ import Swiper from 'swiper';
 import "swiper/css/swiper.min.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import {swiperAnimateCache, swiperAnimate, clearSwiperAnimate} from 'swiper-animate-cn';
-import { WOW } from './wow';
 import '../css/index.css';
+import { WOW } from './wow';
 // <!--侧滑--搜索-->
         $(function () {
             $(".btn_phoneMenu").click(function () {
@@ -121,3 +121,64 @@ import '../css/index.css';
           var wow=new WOW({boxClass:'wow',animateClass:'animated',offset:150,mobile:true,live:true});
           wow.init();
       })
+
+
+      //表单提交
+      
+        $(function() {
+
+            $("#submit").click(function() {
+                var isPhone = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
+
+                var isTel = /^(?:(?:0\d{2,3})-)?(?:\d{7,8})(-(?:\d{3,}))?$/
+
+                var name = $("#your-name").val();
+                var email = $("#your-email").val();
+                var tel = $("#your-tel").val();
+
+                var con = $(".con").val();
+                var pattern = new RegExp("[~'!@#$%^&*()-+_=:]");  
+                if (name == "") {
+                    alert("请输入您的姓名");
+                    return false;
+                }
+
+
+                if (pattern.test(name)) {
+                    alert("姓名带有非法字符！");
+                    return false;
+                }  
+
+
+                if (tel == "") {
+                    alert("请输入您的联系电话");
+                    return;
+                }
+                if (isPhone.test(tel) || isTel.test(tel)) {
+
+                }else {
+                    alert("请输入正确的电话号码");
+                    return;
+                }
+
+                $.ajax({
+                    url: 'request/request.php',
+                    data: {name: name, email: email, tel: tel, con: con},
+                    cache: false,
+                    type: 'POST',
+                    success: function(data) {
+                        if (data) {
+                            alert("留言成功，我们会尽快与您取得联系！");
+                        }else{
+                            alert("服务器开小差，请您直接联系客户！");
+                        }
+                    }
+                })
+
+            })
+
+        });
+
+  
+    
+
